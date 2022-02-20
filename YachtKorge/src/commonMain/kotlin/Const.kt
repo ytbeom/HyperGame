fe88d266a829551ef64e9b71de9c764f880822f1
Taekbeom Yoo <ytbeom@gmail.com>
2022-02-21 01:51:02 +0900

    Create Const.kt

---
+++ YachtKorge/src/commonMain/kotlin/Const.kt
@@ -0,0 +1,7 @@
+const val WIDTH = 1600
+const val HEIGHT = 900
+const val WHITE = "#ffffff"
+const val BLACK = "#000000"
+const val MAIN_COLOR = "#00583a"
+const val SUB_COLOR = "#008256"
+const val fileName = "test"
